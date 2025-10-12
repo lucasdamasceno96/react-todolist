fc3b958a5c93292a2cbe42f6ae524097085e423f
lucasdamasceno96 <ldp21k@outlook.com>
2025-10-12 13:06:00 -0300

    feat: task completed and task deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: 'Doctors Appointment',
+    description: 'Need to go to the doctors at 5pm',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    title: 'Meeting at School',
+    description: 'Meeting with the new client at school',
+    isCompleted: true
+  }])
+
+  function onTaskClick(taskId){
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId){
+        return {...task, isCompleted: !task.isCompleted}
+      }
+      return task})
+      setTasks(newTasks)
+    }
+  
+  function onDeleteTaskClick(taskId){
+  const newTasks = tasks.filter(task => task.id !== taskId)
+  setTasks(newTasks)
+  }
+    
   return(
- <div>
-  <h1> Task Management </h1>
-  <Tasks />
+ <div className="w-screen h-screen bg-slate-700 flex justify-center p-6">
+  <div className="w-[500px]"> 
+  <h1 className="text-3xl text-slate-100 font-bold text-center"> Task Management </h1>
+  
+  <Tasks  tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
   <AddTask />
+  </div>
  </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,17 @@
-function Tasks(){
-    return <h1>tasks</h1>
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+  return (
+    <ul className="flex flex-col p-6 gap-2 my-4 bg-slate-400 rounded-md shadow-md">
+      {tasks.map((task, index) => (
+        <li key={task.id} className="flex gap-2">
+        <button onClick={() => onTaskClick(task.id)} className={`bg-slate-100 text-left w-full text-purple-600 font-bold p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}>  {task.title} </button>
+        <button className="bg-blue-400 text-left text-white px-2 py-1 rounded-md float-right">Details</button>
+           <button className="bg-red-400 text-left text-white px-2 py-1 rounded-md float-right" onClick={() => onDeleteTaskClick(task.id) }  >Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Tasks
+export default Tasks;
