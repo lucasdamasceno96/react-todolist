a64f3ee0947db9778c51690f3ad9e250b07bc9bd
lucasdamasceno96 <ldp21k@outlook.com>
2025-10-13 22:01:00 -0300

    feat: react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from './App.jsx';
+import './index.css';
+import TaskPage from './pages/TaskPage.jsx';
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "task",
+    element: <TaskPage />,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,9 @@
+function TaskPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-4">Task Manager</h1>
+    </div>
+  );
+}
+
+export default TaskPage;
